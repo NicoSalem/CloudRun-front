40ad1ff04012335a70796427fb215ab7fa39c17c
nicholas Salem <nsalem@u.rochester.edu>
2022-06-19 23:49:45 -0400

    first commit

---
+++ src/App.js
@@ -0,0 +1,28 @@
+
+import './App.css';
+
+
+
+function App() {
+  return (
+    <div className="App">
+      <h1>Hello world</h1>
+      <button onClick={handleClick}>click me</button>
+    </div>
+  );
+}
+
+
+const handleClick =  async () => {
+  console.log(await basicFetch("https://api.kanye.rest/"))
+  console.log('this is:');
+  console.log(await basicFetch("http://localhost:3003/j"))
+}
+
+const basicFetch = async (endpoint) => {
+  const req = await fetch(endpoint);
+  const json = req.json();
+  return json;
+}
+
+export default App;
