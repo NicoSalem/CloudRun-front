9545676025a258aaf38ee0843c2c3218a7a4e2d5
nicholas Salem <nsalem@u.rochester.edu>
2022-06-20 14:08:12 -0400

    attempt 2 cloud build cicdA

--- cloudrun.tf
+++ cloudrun.tf
@@ -21,9 +21,9 @@ data "google_iam_policy" "noauth" {
 }
 
 resource "google_cloud_run_service_iam_policy" "noauth" {
-  location    = google_cloud_run_service.default.location
-  project     = google_cloud_run_service.default.project
-  service     = google_cloud_run_service.default.name
+  location    = "us-central1"
+  project     = "groovy-autumn-290918"
+  service     = "cloudrun-srv"
 
   policy_data = data.google_iam_policy.noauth.policy_data
 }
