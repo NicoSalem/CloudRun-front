5e64b32d12da9ab023117c56b1b3f2e443d7fdea
nicholas Salem <nsalem@u.rochester.edu>
2022-07-07 20:29:44 -0400

    first commit

--- src/App.js
+++ src/App.js
@@ -17,7 +17,7 @@ function App() {
 const handleClick =  async () => {
   console.log(await basicFetch("https://api.kanye.rest/"))
   console.log('this is:');
-  console.log(await basicFetch("http://localhost:3003/j"))
+  console.log(await basicFetch("https://cloudrun-cicd1-back-svuotfutja-uc.a.run.app/j"))
 }
 
 const basicFetch = async (endpoint) => {
