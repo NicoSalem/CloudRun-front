c7e634ad60035d164616c03278b2d570b257eb71
nicholas Salem <nsalem@u.rochester.edu>
2022-07-11 16:20:26 -0400

    first commit

--- src/App.js
+++ src/App.js
@@ -1,6 +1,32 @@
 
 import './App.css';
 
+/**
+ * setting up publish method for pub sub
+ */
+ const topicNameOrId = 'projects/groovy-autumn-290918/topics/my-first-topic';
+ const data = JSON.stringify({myMessage: 'this is my pubsub message'});
+ 
+ // Imports the Google Cloud client library
+ const {PubSub} = require('@google-cloud/pubsub');
+ 
+ // Creates a client; cache this for further use
+ const pubSubClient = new PubSub();
+ 
+ async function publishMessage() {
+   // Publishes the message as a string, e.g. "Hello, world!" or JSON.stringify(someObject)
+   const dataBuffer = Buffer.from(data);
+ 
+   try {
+     const messageId = await pubSubClient
+       .topic(topicNameOrId)
+       .publishMessage({data: dataBuffer});
+     console.log(`Message ${messageId} published.`);
+   } catch (error) {
+     console.error(`Received error while publishing: ${error.message}`);
+     process.exitCode = 1;
+   }
+ }
 
 
 function App() {
@@ -18,6 +44,7 @@ const handleClick =  async () => {
   console.log(await basicFetch("https://api.kanye.rest/"))
   console.log('this is:');
   console.log(await basicFetch("https://cloudrun-cicd1-back-svuotfutja-uc.a.run.app/db"))
+  publishMessage();
 }
 
 const basicFetch = async (endpoint) => {
@@ -25,5 +52,9 @@ const basicFetch = async (endpoint) => {
   const json = req.json();
   return json;
 }
+
+
+
+publishMessage();
 
 export default App;
